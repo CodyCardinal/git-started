1f3035458522ec2bc6fa477324c895654c17da65
DESKTOP-TO2HP4J\Cody <cardinalc@gmail.com>
2022-07-23 13:12:51 -0700

    added jokes

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
+
+jokes = [
+    "Q: Relationship status? A: I'll leave the relations to the database.",
+    "Q: What do you call an idle server? A: A waiter.",
+    "Q: How many Prolog programmers does it take to change a lightbulb? A: Yes.",
+    "Q: Why was the developer unhappy at their job? A: They wanted arrays."
+
+]
 @app.get("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-# Really just adding this line to incur a change I can push as a test.
+def tell_a_joke():
+    joke = random.choice(jokes)
+    return f'<p>{joke}</p>'
