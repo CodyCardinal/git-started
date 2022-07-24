df3b98d8917f99a8768c5474b384385bd569da4d
DESKTOP-TO2HP4J\Cody <cardinalc@gmail.com>
2022-07-23 18:30:33 -0700

    finished new data layer

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 from flask import Flask
+from data import jokes
 import random
 
 app = Flask(__name__)
 
 
-jokes = [
-    "Q: Relationship status? A: I'll leave the relations to the database.",
-    "Q: What do you call an idle server? A: A waiter.",
-    "Q: How many Prolog programmers does it take to change a lightbulb? A: Yes.",
-    "Q: Why was the developer unhappy at their job? A: They wanted arrays."
 
-]
+
 @app.get("/")
 def tell_a_joke():
     joke = random.choice(jokes)
